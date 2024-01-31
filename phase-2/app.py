23e0834757236d23ddd881085b93ebfcb4e3d7bb
DEV ANAND <153504607+DEVANAND-JAYARAMAN@users.noreply.github.com>
2024-01-31 13:43:01 +0530

    App.py

--- phase-2/app.py
+++ phase-2/app.py
@@ -1,1 +1,15 @@
+pip install flask
+
+from flask import Flask, render_template
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    current_time = time.strftime("%H:%M:%S")
+    return render_template('index.html', current_time=current_time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
